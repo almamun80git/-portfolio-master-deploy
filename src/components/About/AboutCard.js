b33a7afacb492f49749615f3c07362cd53a453a9
Sayeed Al Mamun <0252210004081036@stamford.university>
2025-01-03 12:53:05 +0600

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,23 +8,22 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi, I am <span className="purple">Sayeed</span>
+            from <span className="purple"> Dhaka, Bangladesh.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a self oriented developer.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed BBA in HRM.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Gaming
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Writing journals
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
